eba027a07c6358b050b2f6ca5ffa4d3ed2e743f7
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-03 23:43:02 -0300

    variable de entorno

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%-n^r(4ikt=vgd+%-^ze%#e4)@k1=v=@nkz(2y+60zoe)q0kh'
+from decouple import config
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
