3b475d96e5d6d214ccaa288111f2f0c1abc474c4
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-15 21:52:52 -0300

    Formulario de Login

--- todolist/usuarios/views.py
+++ todolist/usuarios/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import View, TemplateView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 class HomeView(TemplateView):
@@ -25,3 +25,24 @@ class RegistroView(View):
                 messages.error(request, form.error_messages[msg])
             return render(request, 'registro.html', {'form':form})
                     
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('home')
+
+
+def loginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get('username')
+            passw = form.cleaned_data.get('password')
+            usuario = authenticate(username=nombre_usuario, password=passw)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('home')
+            else:
+                messages.error(request, 'El usuario no es válido!')    
+        else:
+            messages.error(request, 'El usuario no es válido!')    
+    form=AuthenticationForm
+    return render(request, 'login.html', {'form':form})
