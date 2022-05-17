baf308fdf164aa1d8da34e0bae2ba653aae084fd
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-17 20:28:37 -0300

    Agrego messages de Django

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as msgs_error
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOGIN_REDIRECT_URL = 'home'
 # LOGOUT_REDIRECT_URL = 'home'
+
+MESSAGE_TAGS = {
+
+    msgs_error.DEBUG : 'debug',
+    msgs_error.INFO : ' info',
+    msgs_error.SUCCESS : 'success',
+    msgs_error.WARNING : 'warning',
+    msgs_error.ERROR : 'error',
+
+}
