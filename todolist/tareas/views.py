f0c40032583c6af0bf712580fd16f564e3bb99bf
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-21 10:06:33 -0300

    Vista para listar tareas

--- todolist/tareas/views.py
+++ todolist/tareas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . models import Tarea
 
-# Create your views here.
+
+class TareasListView(ListView):
+    model = Tarea
+    template_name = "listado.html"
+    context_object_name = 'listado'
+
